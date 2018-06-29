e0c8824b4430e33d2cd56b1e138594f06fa4948b
Evandro Mendonça <evandromendonca@Evandros-MacBook-Pro.local>
2018-06-29 09:32:01 +0100

    first commit

---
+++ collect.py
@@ -0,0 +1,48 @@
+# encoding: utf-8 
+
+import csv
+from collection_data import collection_data
+from datetime import datetime
+import xml.etree.ElementTree as ET
+
+def read_file():
+    collect_data = []
+
+    # read the csv of the collected data and store it in a list of
+    # collect_objects, each object is a row in the csv
+    with open("dados_residuos.csv") as file:
+        reader = csv.reader(file, delimiter=";")
+        reader.next()
+        for row in reader:
+            data = collection_data(row) 
+            collect_data.append(data)
+    
+    # sort the built list
+    collect_data = sorted(collect_data,key=lambda o: o.group)
+
+def do():
+    tree = ET.parse('map.osm')
+    root = tree.getroot()
+    
+    #all = root.findall(u'./way/tag[@v="Calçada dos Mestres"]/..')
+    all = root.findall(u'./way/tag[@v="Rua da Artilharia 1"]/..')
+
+    # for item in all:
+    #     print item.get('id')
+    #     street = item.find('./tag[@k="name"]')
+    #     print street.attrib
+
+    artilharias = root.findall(u'./way/tag[@v="Rua da Artilharia 1"]/..')
+    padre = root.findall(u'./way/tag[@v="Rua Padre António Vieira"]/..')
+    sampaio = root.findall(u'./way/tag[@v="Rua Sampaio e Pina"]/..')
+
+if __name__ == "__main__":
+    read_file()    
+    do()
+
+tree = ET.parse('map.osm')
+root = tree.getroot()
+
+artilharias = root.findall(u'./way/tag[@v="Rua da Artilharia 1"]/..')
+padre = root.findall(u'./way/tag[@v="Rua Padre António Vieira"]/..')
+sampaio = root.findall(u'./way/tag[@v="Rua Sampaio e Pina"]/..')
---
+++ truck.py
@@ -0,0 +1,5 @@
+class truck:
+    def __init__(self, plate, capacity):
+        self.plate = plate
+        self.capacity = capacity
+        self.used_capacity = 0
