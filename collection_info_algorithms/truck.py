0b436f54ab3c025a1ac2a5477326fc4e012b440c
Evandro Mendonça <evandro.mendonca@tradingeconomics.com>
2018-09-10 19:08:23 +0100

    sota

--- collection_info_algorithms/truck.py
+++ collection_info_algorithms/truck.py
@@ -8,12 +8,12 @@ class truck:
     
     def capacity_desc_to_m3(self, cap_text):
         if cap_text == 'Remoção 14/16 m3':
-            14
+            return 14
         elif cap_text == 'Remoção 9 m3 - Mini':
-            9
+            return 9
         elif cap_text == 'Remoção 11 m3 - Maxi':
-            11
+            return 11
         elif cap_text == 'Remoção 9 m3 - Médio':
-            9
+            return 9
         elif cap_text == 'Remoção 7 m3':
-            7    
+            return 7    
